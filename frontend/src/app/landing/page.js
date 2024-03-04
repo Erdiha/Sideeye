054ba9270e46d5fd2076942b213e9089fc1b6c94
erdiha <erdiha@hotmail.com>
2024-03-04 11:26:30 -0800

    signup and login page initial commit

---
+++ frontend/src/app/landing/page.js
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <div className="bg-red-200 h-screen flex flex-col justify-center items-center">
+      <h1>HEY</h1>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ frontend/src/ui/Icon.js
@@ -0,0 +1,19 @@
+const Icon = () => {
+  return (
+    <div className=" items-center flex w-fit   ">
+      <span className="  flex border-white  ">sideEYE</span>
+      {/* <Image
+        style={{
+          borderRadius: "20%",
+          display: "flex",
+          backgroundColor: "transparent",
+        }}
+        height={70}
+        width={70}
+        src="/sideeye2.png"
+      ></Image> */}
+    </div>
+  );
+};
+
+export default Icon;
